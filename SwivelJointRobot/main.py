70e639252188b7521e635aaf84a6c4a784cc6051
kevin-sekol <125811002+kevin-sekol@users.noreply.github.com>
2025-01-01 18:45:14 +0100

    Setting up the project

---
+++ SwivelJointRobot/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+
+# Create your objects here
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize a motor at port B.
+test_motor = Motor(Port.B)
+
+# Write your program here
+
+# Play a sound.
+ev3.speaker.beep()
+
+# Run the motor up to 500 degrees per second. To a target angle of 360 degrees.
+for i in range(10):
+    test_motor.run_target(500, 3600)
+
+for i in range(10):
+    test_motor.run_target(500, -3600)
+
+# Play another beep sound.
+ev3.speaker.beep(frequency=1000, duration=500)
