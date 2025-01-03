2fb47daa7f89e3ef3d6e6d8dfb136dba1b7c103b
kevin-sekol <125811002+kevin-sekol@users.noreply.github.com>
2025-01-03 16:54:56 +0100

    Communication with remote set up

--- SwivelJointRobot/main.py
+++ SwivelJointRobot/main.py
@@ -21,6 +21,9 @@ ev3 = EV3Brick()
 base_module_motor = Motor(Port.A)
 second_module_motor = Motor(Port.B)
 
+# Initialize remote control
+receiver = InfraredSensor(Port.S1)
+
 # Write your program here
 
 # Play a sound.
@@ -28,18 +31,27 @@ ev3.speaker.beep()
 
 # First parameter: speed (Degrees per second), second parameter: target angle
 # Base module
-for i in range(10):
-    base_module_motor.run_target(500, 3600)
+#for i in range(10):
+    #base_module_motor.run_target(500, 3600)
 
-for i in range(10):
-    base_module_motor.run_target(500, -3600)
+#for i in range(10):
+    #base_module_motor.run_target(500, -3600)
 
 # Second module
-for i in range(10):
-    second_module_motor.run_target(500, 3600)
+#for i in range(10):
+    #second_module_motor.run_target(500, 3600)
 
-for i in range(10):
-    second_module_motor.run_target(500, -3600)
+#for i in range(10):
+    #second_module_motor.run_target(500, -3600)
+
+pressed_buttons = []
+while len(pressed_buttons) < 11:
+    if receiver.keypad() != []:
+        pressed_buttons.append(receiver.keypad())
+
+print(pressed_buttons)
+print(receiver.keypad())
 
 # Play another beep sound.
 ev3.speaker.beep(frequency=1000, duration=500)
+
